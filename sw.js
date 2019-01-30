26cb74f9a191d5510cc3b82fd728767910dd00e2
zemlyansky <anton.zemlyansky@gmail.com>
2019-01-30 14:17:35 +0200

    v0.10.4 (service worker)

---
+++ sw.js
@@ -0,0 +1,111 @@
+const version = 'v0.10.4a'
+const assets = [
+  'dist/density-plot.js',
+  'dist/bundle.js',
+  'dist/webppl.min.js',
+  'dist/worker.js',
+  'css/vue-material.css',
+  'css/handsontable.full.min.css',
+  'css/vue2-perfect-scrollbar.min.css',
+  'css/roboto.css',
+  'css/icons.css',
+  'css/vis-network.min.css',
+  'css/img/network/acceptDeleteIcon.png',
+  'css/img/network/addNodeIcon.png',
+  'css/img/network/backIcon.png',
+  'css/img/network/connectIcon.png',
+  'css/img/network/cross2.png',
+  'css/img/network/cross.png',
+  'css/img/network/deleteIcon.png',
+  'css/img/network/downArrow.png',
+  'css/img/network/editIcon.png',
+  'css/img/network/leftArrow.png',
+  'css/img/network/minus.png',
+  'css/img/network/plus.png',
+  'css/img/network/rightArrow.png',
+  'css/img/network/upArrow.png',
+  'css/img/network/zoomExtends.png',
+  'images/android-icon-192x192.png',
+  'images/favicon-16x16.png',
+  'images/favicon-32x32.png',
+  'images/favicon-96x96.png',
+  'images/favicon.ico',
+  'images/s.png',
+  'fonts/MaterialIcons-Regular.ttf',
+  'fonts/MaterialIcons-Regular.woff',
+  'fonts/MaterialIcons-Regular.woff2',
+  'fonts/Roboto-Bold.ttf',
+  'fonts/Roboto-Bold.woff',
+  'fonts/Roboto-Bold.woff2',
+  'fonts/Roboto-Light.ttf',
+  'fonts/Roboto-Light.woff',
+  'fonts/Roboto-Light.woff2',
+  'fonts/Roboto-Medium.ttf',
+  'fonts/Roboto-Medium.woff',
+  'fonts/Roboto-Medium.woff2',
+  'fonts/Roboto-Regular.ttf',
+  'fonts/Roboto-Regular.woff',
+  'fonts/Roboto-Regular.woff2'
+]
+
+console.log('This of SW:', this)
+
+self.addEventListener('install', function (event) {
+  console.log('Service worker: Installation started')
+  event.waitUntil(
+    caches
+      .open(version)
+      .then(function (cache) {
+        return cache.addAll(assets)
+      })
+      .then(function () {
+        console.log(`Service worker: Completed. Cached ${assets.length} assets`)
+      })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  console.log('Service worker: fetch event', event)
+  if (event.request.method !== 'GET') {
+    console.log('Service worker: event ignored', event.request.method, event.request.url)
+    return
+  }
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function (cached) {
+        // Load from network anyway
+        const networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve)
+
+        console.log('Service worker: fetch', cached ? '(cached)' : '(network)', event.request.url)
+        return cached || networked
+
+        function fetchedFromNetwork (response) {
+          const cacheCopy = response.clone()
+          console.log('Service worker: fetch response from network', event.request.url)
+          caches
+            .open(version)
+            .then(function add (cache) {
+              cache.put(event.request, cacheCopy)
+            })
+            .then(function () {
+              console.log('Service worker: fetch response stored in cache', event.request.url)
+            })
+          return response
+        }
+
+        function unableToResolve () {
+          console.log('Service worker: fetch request failed in both cache and network')
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          })
+        }
+      }) // *caches.match.then
+  ) // *respondwith
+})
