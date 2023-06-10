3a8b2c8f4dca967228c366f50a5e5c28a7059f72
zemlyansky <anton.zemlyansky@gmail.com>
2023-06-10 19:40:33 +0200

    Test main functionality and examples

---
+++ test-examples.test.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+require('expect-puppeteer')
+
+page.setDefaultTimeout(10000)
+const port = 8080
+const urlModel = (name) => `http://localhost:${port}/?m=${name}`
+
+// Get all model names from `example` folder
+const modelNames = fs
+  .readdirSync(path.join(__dirname, './examples'))
+  .filter(name => name.endsWith('.json'))
+  .map(name => name.replace('.json', ''))
+
+describe('Examples', () => {
+  // Load a model and check if it has a block, run it and check the result
+  test.each(modelNames)('Check: http://localhost:8080/?m=%s', async (modelName) => {
+    await page.setViewport({ width: 1920, height: 1080 })
+    await page.goto(urlModel(modelName))
+    await page.waitForSelector('.block')
+    await page.screenshot({ path: `./tmp/screenshot_all_${modelName}_1.png` })
+    await expect(page).toClick('#btn-run')
+    await page.waitForSelector('.result-header', { timeout: 20000 })
+    await page.screenshot({ path: `./tmp/screenshot_all_${modelName}_2.png` })
+  }, 20000)
+})
