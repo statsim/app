06b25de108a765434fb043537b81448b128ae774
zemlyansky <anton.zemlyansky@gmail.com>
2023-06-28 14:50:03 +0200

    Some examples can be flow-based, so remove .block dependency in the test

--- test-examples.test.js
+++ test-examples.test.js
@@ -17,7 +17,7 @@ describe('Examples', () => {
   test.each(modelNames)('Check: http://localhost:8080/?m=%s', async (modelName) => {
     await page.setViewport({ width: 1920, height: 1080 })
     await page.goto(urlModel(modelName))
-    await page.waitForSelector('.block')
+    await page.waitForSelector('#btn-run', { timeout: 2000 }) // Change to btn-run as .block is not always present (e.g. flow)
     await page.screenshot({ path: `./tmp/screenshot_all_${modelName}_1.png` })
     await expect(page).toClick('#btn-run')
     await page.waitForSelector('.result-header', { timeout: 20000 })
